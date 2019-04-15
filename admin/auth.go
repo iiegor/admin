587546d326e321f61f65d24c8fb107721dd61b8d
Iegor Azuaga <iegorazuaga@gmail.com>
2019-04-16 00:39:00 +0200

    use admin.ApiPrefix when defining login route

--- admin/auth.go
+++ admin/auth.go
@@ -2,12 +2,13 @@ package admin
 
 import (
 	"net/http"
+	"path"
 
 	"github.com/julienschmidt/httprouter"
 )
 
 func (admin Admin) Auth() {
-	admin.router.POST("/api/login", func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	admin.router.POST(path.Join(admin.ApiPrefix, "login"), func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 		w.Write([]byte("{\"firstName\": \"Iegor\", \"lastName\": \"Azuaga\", \"nickName\": \"iegor\" }"))
 	})
 }
